16b9b0b9f8a51ae951430ed5e4647743a9f05fa2
Marshall McQuillen <marshallm94@gmail.com>
2018-06-16 16:49:18 -0600

    age imputation complete

--- titanic_survival_classification/ml_python.py
+++ titanic_survival_classification/ml_python.py
@@ -17,16 +17,73 @@ from sklearn.svm import SVC
 plt.style.use('ggplot')
 
 
-def clean_data(df):
+def format_data(df, title_options=set(['Capt.','Col.','Don.','Dr.','Major.','Master.','Miss.','Mr.','Mrs.','Ms.','Rev.'])):
+    '''
+    Converts the Pclass, SibSp and Parch columns to be objects, as well as
+    creates a new column, "Title", that is the prefix associated with each
+    passenger (i.e. Mr., Mrs., Master., etc.)
+
+    Parameters:
+    ----------
+    df : (Pandas DataFrame)
+        Must contain columns Pclass, SibSp, Parch and Name.
+    title_options : (set)
+        A set of possible strings that a passenger could have been called.
+        (i.e. Master., Dr., Miss., etc.)
+
+    Returns:
+    ----------
+    None
+    '''
     df['Pclass'] = df['Pclass'].astype(object)
+    df['SibSp'] = df['SibSp'].astype(object)
+    df['Parch'] = df['Parch'].astype(object)
+    df['Split'] = df['Name'].apply(lambda x: x.split())
+    df['Title'] = df['Split'].apply(lambda x: title_options.intersection(x).pop() if len(title_options.intersection(x)) > 0 else 'Misc.')
+    df['Title'] = df['Title'].astype(object)
+    df.drop('Split', axis=1, inplace=True)
+
+
+def impute_age(df):
+    '''
+    Imputes the mean age based on subgroups defined by all possible
+    combinations of the unique values in the Sex, Pclass and Title
+    columns.
+
+    Parameters:
+    ----------
+    df : (Pandas DataFrame)
+        Must include columns "Title", "Pclass", "Sex" and "Age"
+
+    Returns:
+    ----------
+    None
+    '''
+    check = set()
+    for i in np.unique(df['Title']):
+        mask = df['Title'] == i
+        percent_nan = count_nans(df[mask], ['Age'], verbose=False)[0][1]
+        if percent_nan > 0:
+            check.add(i)
+
+
+    for pclass in np.unique(df['Pclass']):
+        for sex in np.unique(df['Sex']):
+            for title in check:
+                mask = df['Pclass'] == pclass
+                mask2 = df['Sex'] == sex
+                mask3 = df['Title'] == title
+                percent_nan = count_nans(df[mask & mask2 & mask3], ['Age'], verbose=False)[0][1]
+                if percent_nan > 0:
+                    imputer = Imputer(strategy='mean')
+                    df.loc[mask & mask2 & mask3, 'Age'] = imputer.fit_transform(df.loc[mask & mask2 & mask3, 'Age'].values.reshape(-1,1))
+
+def create_X_y():
     df.drop(['Name','Ticket','Cabin'], axis=1, inplace=True)
     df = pd.get_dummies(df)
 
     X = df.drop('Survived', axis=1, inplace=False).values
     y = df['Survived'].values
-
-    impute = Imputer()
-    X = impute.fit_transform(X)
 
     return X, y
 
@@ -88,90 +145,62 @@ if __name__ == "__main__":
 
     # data cleaning
     df = pd.read_csv("/Users/marsh/data_science_projects/Kaggle_Competitions/titanic_survival_classification/titanic_train.csv")
-    col_nans = count_nans(df, df.columns)
-
-    df['Title'] = df['Name'].apply(lambda x: x.split()[1])
-    title_options = []
-    for i in np.unique(df['Title']):
-        if i[-1] == ".":
-            title_options.append(i)
-
-    double_check = set()
-    for i in title_options:
-        mask = df['Title'] == i
-        percent_nan = count_nans(df[mask], ['Age'], verbose=False)[0][1]
-        if percent_nan > 0:
-            double_check.add(i)
-
-
-    for pclass in np.unique(df['Pclass']):
-        for sex in np.unique(df['Sex']):
-            for title in double_check:
-                mask = df['Pclass'] == pclass
-                mask2 = df['Sex'] == sex
-                mask3 = df['Title'] == title
-                percent_nan = count_nans(df[mask & mask2 & mask3], ['Age'], verbose=False)[0][1]
-                if percent_nan > 0:
-                    print(df[mask & mask2 & mask3].shape, "Pclass: {} | Sex: {} | Title: {} | Age: {:.2f}% Nan".format(pclass, sex, title, percent_nan*100))
-                    imputer = Imputer(strategy='median')
-                    df.loc[mask & mask2 & mask3, 'Age'] = imputer.fit_transform(df.loc[mask & mask2 & mask3, 'Age'].reshape(-1,1))
-                    # med = df.loc[mask & mask2 & mask3, 'Age'].median()
-                    # df.loc[mask & mask2 & mask3, 'Age'].fillna(med, inplace=True)
-
-    count_nans(df, df.columns)
+    format_data(df)
+    impute_age(df)
+    count_nans(df, ['Age'])
 
 
 
 
 
-    X, y = clean_data(df)
-
-    standardizer = StandardScaler()
-    standardizer.fit(X)
-    X = standardizer.transform(X)
-
-    # modeling
-    x_train, x_test, y_train, y_test = train_test_split(X, y)
-
-    log_mod = LogisticRegression()
-    rf = RandomForestClassifier()
-    gradient_booster = GradientBoostingClassifier()
-    svc = SVC()
-    dt = DecisionTreeClassifier()
-    bag = BaggingClassifier()
-
-    models = [log_mod, rf, gradient_booster, svc, dt, bag]
-    for model in models:
-        score_model(model, x_train, y_train)
-
-    # coarse grid searching
-    log_mod_grid = {"penalty": ['l1','l2'],
-                    "C": [0.001, 0.01, 0.1, 1, 10],
-                    "class_weight": ['balanced', None]
-    }
-    rf_grid = {"n_estimators": list(np.arange(100, 700, 100)),
-               "criterion": ['gini','entropy'],
-               "max_features": ['auto','log2', None]
-    }
-    gradient_booster_grid = {"learning_rate": [0.001, 0.01, 0.1, 1, 10],
-                             "n_estimators": list(np.arange(100, 700, 100)),
-                             "max_depth": [1,2,3],
-    }
-    svc_grid = {"C": [1, 3, 5, 10],
-                "kernel": ['linear','poly','rbf'],
-                "degree": [2,3,4]
-    }
-    dt_grid = {"criterion": ['gini','entropy'],
-               "class_weight": ["balanced", None]
-    }
-    bag_grid = {"n_estimators": list(np.arange(100, 700, 100)),
-                "max_features": [0.25, 0.5, 0.75, 1.0]
-    }
-
-    grids = [log_mod_grid, rf_grid, gradient_booster_grid, svc_grid, dt_grid, bag_grid]
-
-    model_grids = zip(models, grids)
-    for model, grid in model_grids:
-        g = GridSearchCV(model, n_jobs=-1, scoring='accuracy', cv=5, param_grid=grid, verbose=True)
-        g.fit(x_train, y_train)
-        score_model(g.best_estimator_, x_train, y_train)
+    # X, y = clean_data(df)
+    #
+    # standardizer = StandardScaler()
+    # standardizer.fit(X)
+    # X = standardizer.transform(X)
+    #
+    # # modeling
+    # x_train, x_test, y_train, y_test = train_test_split(X, y)
+    #
+    # log_mod = LogisticRegression()
+    # rf = RandomForestClassifier()
+    # gradient_booster = GradientBoostingClassifier()
+    # svc = SVC()
+    # dt = DecisionTreeClassifier()
+    # bag = BaggingClassifier()
+    #
+    # models = [log_mod, rf, gradient_booster, svc, dt, bag]
+    # for model in models:
+    #     score_model(model, x_train, y_train)
+    #
+    # # coarse grid searching
+    # log_mod_grid = {"penalty": ['l1','l2'],
+    #                 "C": [0.001, 0.01, 0.1, 1, 10],
+    #                 "class_weight": ['balanced', None]
+    # }
+    # rf_grid = {"n_estimators": list(np.arange(100, 700, 100)),
+    #            "criterion": ['gini','entropy'],
+    #            "max_features": ['auto','log2', None]
+    # }
+    # gradient_booster_grid = {"learning_rate": [0.001, 0.01, 0.1, 1, 10],
+    #                          "n_estimators": list(np.arange(100, 700, 100)),
+    #                          "max_depth": [1,2,3],
+    # }
+    # svc_grid = {"C": [1, 3, 5, 10],
+    #             "kernel": ['linear','poly','rbf'],
+    #             "degree": [2,3,4]
+    # }
+    # dt_grid = {"criterion": ['gini','entropy'],
+    #            "class_weight": ["balanced", None]
+    # }
+    # bag_grid = {"n_estimators": list(np.arange(100, 700, 100)),
+    #             "max_features": [0.25, 0.5, 0.75, 1.0]
+    # }
+    #
+    # grids = [log_mod_grid, rf_grid, gradient_booster_grid, svc_grid, dt_grid, bag_grid]
+    #
+    # model_grids = zip(models, grids)
+    # for model, grid in model_grids:
+    #     g = GridSearchCV(model, n_jobs=-1, scoring='accuracy', cv=5, param_grid=grid, verbose=True)
+    #     g.fit(x_train, y_train)
+    #     score_model(g.best_estimator_, x_train, y_train)
