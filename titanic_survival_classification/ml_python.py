92174b04a9f93782bffb732c77e1d25c4eef3ae2
Marshall McQuillen <marshallm94@gmail.com>
2018-06-16 14:57:33 -0600

    grouped imputing cmoplete

--- titanic_survival_classification/ml_python.py
+++ titanic_survival_classification/ml_python.py
@@ -31,13 +31,34 @@ def clean_data(df):
     return X, y
 
 
-def count_nans_df(df):
-    nans = []
-    for i in df.select_dtypes(exclude=['object']).columns:
-        print(i, np.isnan(df[i].values).sum())
-        nans.append(np.isnan(df[i].values).sum())
+def count_nans(df, columns, verbose=True):
+    """
+    Calculates nan value percentages per column in a pandas DataFrame.
 
-    return list(zip(list(df.columns), nans))
+    Parameters:
+    ----------
+    df : (Pandas DataFrame)
+    columns : (list)
+        A list of strings of the columns to check for null values. Note
+        that even if you are checking only one column, it must be
+        contained within a list. (Pass df.columns to check all columns)
+    verbose : (bool)
+        If True (default), prints column names and NaN percentage
+
+    Returns:
+    ----------
+    col_nans : (list)
+        List containing tuples of column names and percentage NaN for
+        that column.
+    """
+    col_nans = []
+    for col in columns:
+        percent_nan = pd.isna(df[col]).sum()/len(pd.isna(df[col]))
+        col_nans.append((col, percent_nan))
+        if verbose:
+            print("{} | {:.2f}% NaN".format(col, percent_nan*100))
+
+    return col_nans
 
 
 def count_nans_array(X):
@@ -67,6 +88,42 @@ if __name__ == "__main__":
 
     # data cleaning
     df = pd.read_csv("/Users/marsh/data_science_projects/Kaggle_Competitions/titanic_survival_classification/titanic_train.csv")
+    col_nans = count_nans(df, df.columns)
+
+    df['Title'] = df['Name'].apply(lambda x: x.split()[1])
+    title_options = []
+    for i in np.unique(df['Title']):
+        if i[-1] == ".":
+            title_options.append(i)
+
+    double_check = set()
+    for i in title_options:
+        mask = df['Title'] == i
+        percent_nan = count_nans(df[mask], ['Age'], verbose=False)[0][1]
+        if percent_nan > 0:
+            double_check.add(i)
+
+
+    for pclass in np.unique(df['Pclass']):
+        for sex in np.unique(df['Sex']):
+            for title in double_check:
+                mask = df['Pclass'] == pclass
+                mask2 = df['Sex'] == sex
+                mask3 = df['Title'] == title
+                percent_nan = count_nans(df[mask & mask2 & mask3], ['Age'], verbose=False)[0][1]
+                if percent_nan > 0:
+                    print(df[mask & mask2 & mask3].shape, "Pclass: {} | Sex: {} | Title: {} | Age: {:.2f}% Nan".format(pclass, sex, title, percent_nan*100))
+                    imputer = Imputer(strategy='median')
+                    df.loc[mask & mask2 & mask3, 'Age'] = imputer.fit_transform(df.loc[mask & mask2 & mask3, 'Age'].reshape(-1,1))
+                    # med = df.loc[mask & mask2 & mask3, 'Age'].median()
+                    # df.loc[mask & mask2 & mask3, 'Age'].fillna(med, inplace=True)
+
+    count_nans(df, df.columns)
+
+
+
+
+
     X, y = clean_data(df)
 
     standardizer = StandardScaler()
